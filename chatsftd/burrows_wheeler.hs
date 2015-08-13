2a70d436c4ccf8acd5b364de507c59022513c4dc
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2015-08-13 16:20:09 +0900

    Fix bugs

--- chatsftd/burrows_wheeler.hs
+++ chatsftd/burrows_wheeler.hs
@@ -1,9 +1,5 @@
-{-
-Note: the source code on the book fails
-      listArray (0,n-1) ===> listArray(0,n)
-      [x | .. ] ==> lrot [x | .. ]
--}
-
+{-# OPTIONS -Wall #-}
+{-  Note: the source code on the book fails -}
 import Data.Array
 import Data.List(sort,tails)
 
@@ -19,15 +15,19 @@ tag xs = xs ++ [eof]
 rrot :: [a] -> [a]
 rrot xs = [last xs] ++ init xs
 
-lrot :: [a] -> [a]
-lrot (x:xs) = xs ++ [x]
-
 transform :: (EOF a, Ord a) => [a] -> ([a],Int)
-transform xs = (lrot[x | i <- ps, let x = xa ! i, x /= eof], k-1)
- where
+transform xs = ([xa!i | i <- ps], k)
+ where 
   n   = length xs
-  k   = length(takeWhile(/=0)ps)
-  xa  = listArray (0,n) (rrot xs')
+  k   = length(takeWhile (/=0) ps)
+  xa  = listArray(0,n) (rrot xs')
   xs' = tag xs
-  ps  = map snd (sort(zip(tails xs')[0..n]))
+  ps  = map snd(sort(zip(tails xs')[0..n]))
+
+untransform :: Ord a => ([a],Int) -> [a]
+untransform (ys,k) = take (n-1) (tail (map (ya!) (iterate (pa!) k)))
+ where
+  n  = length ys
+  ya = listArray (0,n-1) ys
+  pa = listArray (0,n-1) (map snd (sort (zip ys [0..])))
 
