29fe6aad8babdb0f7307292b344dbd0e57e65920
Onkoh Dondo <ideryuhouu@gmail.com>
2015-08-14 09:58:17 +0900

    Added main.hs evaluates Komachi-zan.

---
+++ Komachi/main.hs
@@ -0,0 +1,54 @@
+import Data.List (intercalate)
+
+
+
+type Expression = [Term]
+type Term = [Factor]
+type Factor = [Digit]
+type Digit = Int
+
+valExpr :: Expression -> Int
+valExpr = sum . map valTerm
+
+valTerm :: Term -> Int
+valTerm = product . map valFact
+
+valFact :: Factor -> Int
+valFact = foldl1 (\n d -> 10 * n + d)
+
+good :: Int -> Int -> Bool
+good = (==)
+
+extend :: Digit -> [Expression] -> [Expression]
+extend x [] = [[[[x]]]]
+extend x es = concatMap (glue x) es
+
+glue :: Digit -> Expression -> [Expression]
+glue d ((f:fs):ts) = [((d:f):fs  )       :ts,
+                      ([d]  :f:fs)       :ts,
+		      [[d]       ]:(f:fs):ts ]
+
+expressions :: Int -> [Digit] -> [Expression]
+expressions n = filter (good n . valExpr) . foldr extend []
+
+--main = return ()
+--main = print $ valExpr [[[1,2]],[[3,4]],[[5],[6]],[[7]],[[8]],[[9]]]
+--main = print $ expressions [1,2,3,4,5,6,7,8,9]
+main = do l <- getLine
+          let [a, i] = map read $ words l
+          putStrLn $ unlines $ map showExpr $ expressions a [1..i]
+
+---------Display----------
+
+showExpr :: Expression -> String 
+--showExpr [t]    = showTerm t
+--showExpr (t:ts) = showTerm t ++ " + " ++ showExpr ts
+showExpr = intercalate " + " . map showTerm
+
+showTerm :: Term -> String
+--showTerm [f]    = showFctr f
+--showTerm (f:fs) = showFctr f ++ "*" ++ showTerm fs
+showTerm = intercalate "*" . map showFctr
+
+showFctr :: Factor -> String
+showFctr = foldl1 (++) . map show
